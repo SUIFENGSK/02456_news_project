330646702a88f467ac79a0b838b0d454f64e1dbc
Andreas Lildballe <s214387@dtu.dk>
2024-12-02 12:42:04 +0100

    Update main.py

--- src/torch_nerd/main.py
+++ src/torch_nerd/main.py
@@ -1,23 +1,36 @@
-# %%
-BLACKHOLE = True
-
-import os
-import sys
+from from_ebrec.evaluation import AucScore, MrrScore, NdcgScore
+from from_ebrec.evaluation import MetricEvaluator
+import matplotlib.pyplot as plt
+from tqdm import tqdm  # for progress bars
+import torch.optim as optim
+import torch.nn as nn
+from hyperparameters import hparams_nrms
+from nrms import NRMSModel
+from dataloader import NRMSDataLoader
+from from_ebrec._articles import create_article_id_to_value_mapping
+from from_ebrec._articles import convert_text2encoding_with_transformers
+from from_ebrec._polars import concat_str_columns
+from from_ebrec._nlp import get_transformers_word_embeddings
+import transformers as huggingface
+import from_ebrec._constants as cs
+from utils.data_handler import NewsDataset
+import torch
 from pathlib import Path
+import sys
+import os
+BLACKHOLE = False
+
 
 if BLACKHOLE:
     workspace_path = os.path.expandvars('$BLACKHOLE')
-    sys.path.append(workspace_path + '/DeepLearning/02456_news_project/src')
-    DATAPATH = Path(workspace_path + "/DeepLearning/ebnerd_data").expanduser()
+    sys.path.append(workspace_path+'/DeepLearning/02456_news_project/src')
+    DATAPATH = Path(workspace_path+"/DeepLearning/ebnerd_data").expanduser()
 else:
     DATAPATH = Path("~/ebnerd_data").expanduser()
 
 DATASET = "ebnerd_demo"
 # DATASET = "ebnerd_small"
-# %% md
 
-# %%
-import torch
 
 print("torch version:", torch.__version__)
 
@@ -29,14 +42,9 @@ print(device)
 
 # Test:
 # print(torch.zeros(1).cuda())
-# %% md
-
-# %%
-from utils.data_handler import NewsDataset
-import from_ebrec._constants as cs
 
 SEED = 42
-HISTORY_SIZE = 20
+HISTORY_SIZE = 50
 
 COLS = [
     cs.DEFAULT_USER_COL,
@@ -45,24 +53,22 @@ COLS = [
     cs.DEFAULT_HISTORY_ARTICLE_ID_COL,
     cs.DEFAULT_CLICKED_ARTICLES_COL,
     cs.DEFAULT_INVIEW_ARTICLES_COL,
+
+    cs.DEFAULT_ARTICLE_ID_COL,
+    cs.DEFAULT_AGE_COL,
+    cs.DEFAULT_READ_TIME_COL
 ]
 
-# FRACTION = 0.01
-FRACTION = 0.1
+FRACTION = 0.01
+# FRACTION = 0.1
 # FRACTION = 1
 
 # test
 dataset = NewsDataset()
 
-dataset.setup_df(dataset_path=DATAPATH, datasplit=DATASET, history_size=HISTORY_SIZE, columns=COLS, fraction=FRACTION,
-                 seed=SEED)
+dataset.setup_df(dataset_path=DATAPATH, datasplit=DATASET,
+                 history_size=HISTORY_SIZE, columns=COLS, fraction=FRACTION, seed=SEED)
 
-# %%
-import transformers as huggingface
-from from_ebrec._nlp import get_transformers_word_embeddings
-from from_ebrec._polars import concat_str_columns
-from from_ebrec._articles import convert_text2encoding_with_transformers
-from from_ebrec._articles import create_article_id_to_value_mapping
 
 dataset.setup_articles_data(dataset_path=DATAPATH.joinpath(DATASET))
 
@@ -73,20 +79,19 @@ TEXT_COLUMNS_TO_USE = [cs.DEFAULT_SUBTITLE_COL, cs.DEFAULT_TITLE_COL]
 MAX_TITLE_LENGTH = 30
 
 # LOAD HUGGINGFACE:
-transformer_model = huggingface.AutoModel.from_pretrained(TRANSFORMER_MODEL_NAME)
-transformer_tokenizer = huggingface.AutoTokenizer.from_pretrained(TRANSFORMER_MODEL_NAME)
+transformer_model = huggingface.AutoModel.from_pretrained(
+    TRANSFORMER_MODEL_NAME)
+transformer_tokenizer = huggingface.AutoTokenizer.from_pretrained(
+    TRANSFORMER_MODEL_NAME)
 
 word2vec_embedding = get_transformers_word_embeddings(transformer_model)
-df_articles, cat_cal = concat_str_columns(df_articles, columns=TEXT_COLUMNS_TO_USE)
+df_articles, cat_cal = concat_str_columns(
+    df_articles, columns=TEXT_COLUMNS_TO_USE)
 df_articles, token_col_title = convert_text2encoding_with_transformers(
-    df_articles, transformer_tokenizer, cat_cal, max_length=MAX_TITLE_LENGTH
-)
+    df_articles, transformer_tokenizer, cat_cal, max_length=MAX_TITLE_LENGTH)
 article_mapping = create_article_id_to_value_mapping(
-    df=df_articles, value_col=token_col_title
-)
+    df=df_articles, value_col=token_col_title)
 
-# %%
-from dataloader import NRMSDataLoader
 
 BATCH_SIZE = 64
 
@@ -107,9 +112,6 @@ val_dataloader = NRMSDataLoader(
     batch_size=BATCH_SIZE,
 )
 
-# %%
-from nrms import NRMSModel
-from hyperparameters import hparams_nrms
 
 hparams = hparams_nrms()
 
@@ -121,6 +123,10 @@ hparams.history_size = HISTORY_SIZE
 hparams.head_num = 20
 hparams.head_dim = 20
 hparams.attention_hidden_dim = 200
+hparams.linear_hidden_dim = 400
+
+hparams.use_positional_encoding = True
+hparams.use_learned_positions = True
 
 # MODEL OPTIMIZER:
 hparams.optimizer = "adam"
@@ -128,13 +134,11 @@ hparams.loss = "cross_entropy_loss"
 hparams.dropout = 0.2
 hparams.learning_rate = 1e-4
 
-model = NRMSModel(hparams=hparams, word2vec_embedding=word2vec_embedding, seed=SEED, debug=False)
+model = NRMSModel(
+    hparams=hparams, word2vec_embedding=word2vec_embedding, debug=False)
 
 print(model)
 
-# %%
-import torch.nn as nn
-import torch.optim as optim
 
 # Define the loss function and optimizer
 if hparams.loss == "cross_entropy_loss":
@@ -148,9 +152,6 @@ if hparams.optimizer == "adam":
     optimizer = optim.Adam(model.parameters(), lr=hparams_nrms.learning_rate)
 else:
     raise ValueError(f"Optimizer {hparams.optimizer} not supported")
-# %%
-from tqdm import tqdm  # for progress bars
-
 # Train the model
 
 EPOCHS = 10
@@ -215,14 +216,23 @@ for epoch in range(EPOCHS):
     train_loss_history.append(train_loss)
     val_loss_history.append(val_loss)
 
-    print(f"Epoch {epoch + 1}/{EPOCHS}: Train Loss = {train_loss:.4f}, Val Loss = {val_loss:.4f}")
+    print(
+        f"Epoch {epoch + 1}/{EPOCHS}: Train Loss = {train_loss:.4f}, Val Loss = {val_loss:.4f}")
 
-# %%
+# Plot the loss history
+
+plt.plot(train_loss_history, label="Train Loss")
+plt.plot(val_loss_history, label="Val Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
 # Evaluate the model
 BATCH_SIZE_TEST = 1  # Currently onoy supports batch size 1
 
-dataset.setup_test_data(dataset_path=DATAPATH, datasplit=DATASET, history_size=HISTORY_SIZE, columns=COLS,
-                        fraction=FRACTION, seed=SEED)
+dataset.setup_test_data(dataset_path=DATAPATH, datasplit=DATASET,
+                        history_size=HISTORY_SIZE, columns=COLS, fraction=FRACTION, seed=SEED)
 
 test_dataloader = NRMSDataLoader(
     behaviors=dataset.df_test,
@@ -233,8 +243,9 @@ test_dataloader = NRMSDataLoader(
     batch_size=BATCH_SIZE_TEST,
 )
 
-
 # go from [[a], [b], [c]] to [a, b, c]
+
+
 def convert_to_list(l):
     return [(item) for sublist in l for item in sublist]
 
@@ -264,18 +275,17 @@ with torch.no_grad():
 
         pred_test.append(bob)
 
-print("Predictions")
-print(pred_test)
-print("Labels")
-print(dataset.df_test["labels"].to_list())
-
-from from_ebrec.evaluation import MetricEvaluator
-from from_ebrec.evaluation import AucScore, MrrScore, NdcgScore
-
 metrics = MetricEvaluator(
     labels=dataset.df_test["labels"].to_list(),
     predictions=pred_test,
     metric_functions=[AucScore(), MrrScore(), NdcgScore(k=5), NdcgScore(k=10)],
 )
 metrics.evaluate()
-print(metrics)
+number_to_print = 20
+print("Top %d predictions vs labels:" % number_to_print)
+labels = dataset.df_test["labels"].to_list()
+for i in range(number_to_print):
+    print(f"Article {i}")
+    for j in range(len(pred_test[i])):
+        print(f"{pred_test[i][j]:.3f} vs {labels[i][j]:.3f}")
+    print("")
